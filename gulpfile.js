5dd42ed943372bee913adfacad32703a922bc945
Óliver Hierro <ohierro@hiberus.com>
2020-12-20 11:32:27 +0100

    dd

--- gulpfile.js
+++ gulpfile.js
@@ -129,9 +129,46 @@
 // exports.deployPro = deployPro
 // exports.devWatch = devWatch
 
-const browserify = require('browserify');
+var watchify = require('watchify');
+var browserify = require('browserify');
+var gulp = require('gulp');
 const { src, dest, watch } = require('gulp');
 const merge = require('merge-stream');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var log = require('gulplog');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash.assign');
+
+// add custom browserify options here
+var customOpts = {
+  entries: ['./src/web.js'],
+  debug: true,
+  standalone: 'Game',
+  output: 'tmp/bundle.js'
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', log.info); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', log.error.bind(log, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+       // Add transformation tasks to the pipeline here.
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('./dist'));
+}
+
 
 function liveDev() {
     // const js = src('src/*.js')
@@ -139,8 +176,8 @@ function liveDev() {
     const html = src('public/**')
                   .pipe(dest('build/', { overwrite: true }))
 
-    const js = src('tmp/*.js')
-                  .pipe(dest('build/', { overwrite: true }))
+    // const js = src('tmp/*.js')
+    //               .pipe(dest('build/', { overwrite: true }))
 
 
     // const js = src('src/*.js')
@@ -148,7 +185,7 @@ function liveDev() {
     //                 insertGlobals: 'Game'
     //               }))
 
-    return merge(js, html)
+    return merge(html, bundle())
 }
 
 exports.liveDev = liveDev
